42030a1794547b6ddcea4c1436995fb97c70c5ec
GadidhalaKasturi <94217133+GadidhalaKasturi@users.noreply.github.com>
2021-11-23 17:02:57 +0530

    Update and rename documentation_main.md to parking_system.h

---
+++ 3_Implementation/inc/parking_system.h
@@ -0,0 +1,27 @@
+#include "stdio.h"
+#include "parking.h"
+
+struct vehicle *car[2][10] ;
+struct vehicle *scooter[2][10] ;
+int parkinfo[4][10] ;  /* a 2-D array to store number of vehicle parked */
+int vehcount ;  /* to store total count of vehicles */
+int carcount ;	/* stores total count of cars */
+int scootercount ;  /* stores total count of scooters */
+
+/**
+  * creates a 2-D array of car and scooter class 
+*/
+
+void display( ) ;
+void changecol ( struct vehicle * ) ;
+struct vehicle * add ( int, int, int, int ) ;
+void del ( struct vehicle * ) ;
+void getfreerowcol ( int, int * ) ;
+void getrcbyinfo ( int, int, int * ) ;
+void show() ;
+int randint();
+int backupw();
+int backupr();
+struct vehicle * addonstart ( int, int, int, int ) ;
+void total_bill(int ,struct tm *ct1,struct tm *ct2);
+void get_arrival_time(int);
